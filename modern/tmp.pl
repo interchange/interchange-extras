dcf23aac3ef0fdbae5086b7c2fdff84599bbc932
Josh Lavin <digory@cpan.org>
2016-09-21 09:32:12 -0700

    update modern

--- modern/tmp.pl
+++ modern/tmp.pl
@@ -1,32 +1,14 @@
 #!/bin/env perl
 
 use Modern::Perl '2010';
-#use diagnostics;
-use lib 'lib';
 
-## change these:
-my $uid = 123;
-my $gid = 123;
 BEGIN {
 	$ENV{EXT_INTERCHANGE_DIR}     ||= '/path/to/interchange/server';
 	$ENV{EXT_INTERCHANGE_CATALOG} ||= 'catalog_name';
 }
 
-## chuser if root
-use POSIX qw(setuid setgid);
-my $user = $ENV{LOGNAME} || $ENV{USER} || getpwuid($<);
-if ($user eq 'root') {
-	setuid($uid);
-	setgid($gid);
-}
-
-## setup IC environment
+use lib 'lib';
 use Vend::External;
-session();
-chdir $Vend::Cfg->{VendRoot}
-	or die "Unable to chdir $Vend::Cfg->{VendRoot}: $!\n";
-
-## do stuff
 use Vend::MyModule;
 
 my $m = Vend::MyModule->new();
